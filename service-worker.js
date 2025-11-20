33735a7b5294cd7789a3ca5032ec7d0ff68bdaf7
nhwry5213-dotcom <nhwry5213@gmail.com>
2025-11-20 15:40:53 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('yavneel-cache').then(cache => {
+      return cache.addAll([
+        './index.html',
+        './manifest.json',
+        './icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
